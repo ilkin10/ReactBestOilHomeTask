04986bdee58bce7f57875946ef11a29c49026b6d
ilkin <ilkin@MY_ASUS>
2023-08-04 18:24:35 +0400

    Done GasStationHomeTask

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import FuelPricingForm from "./FuelPricingForm";
+import FoodSection from "./FoodSection";
+import TotalPriceOfAllComponent from "./TotalPriceOfAllComponent";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{display:"flex"}}>
+      <div style={{textAlign:"center",margin:"40px"}}>
+        <TotalPriceOfAllComponent></TotalPriceOfAllComponent>
+      </div>
+      <div style={{ width: "50%", float: "left" }}>
+        <FuelPricingForm></FuelPricingForm>
+      </div>
+
+      <div style={{ width: "50%", float: "right" }}>
+        <FoodSection></FoodSection>
+      </div>
     </div>
   );
 }
